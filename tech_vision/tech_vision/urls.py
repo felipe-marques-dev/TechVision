352468e845c4194951b7da6f0ecda953234e073b
Felipe <felipemarques.computacao@gmail.com>
2024-08-27 07:54:02 -0300

    App produtos adicionado na url do projeto

--- tech_vision/tech_vision/urls.py
+++ tech_vision/tech_vision/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuario/', include('usuario.urls'))
+    path('usuario/', include('usuario.urls')),
+    path('produtos/', include('produtos.urls'))
 ]
