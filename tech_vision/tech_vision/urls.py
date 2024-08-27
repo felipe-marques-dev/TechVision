93466eba505166ff697db5ef59fbebb14263753a
Felipe <felipemarques.computacao@gmail.com>
2024-08-27 09:25:20 -0300

    Adição do media no urls.py

--- tech_vision/tech_vision/urls.py
+++ tech_vision/tech_vision/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuario/', include('usuario.urls')),
     path('produtos/', include('produtos.urls'))
 ]
+
+# Adiciona a configuração para servir arquivos de mídia durante o desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
