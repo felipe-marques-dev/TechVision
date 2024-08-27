563a8d5c25f6ae44bfafb1fd058163119217610c
Felipe <felipemarques.computacao@gmail.com>
2024-08-27 08:11:47 -0300

    Inclusao do produtos no INSTALLED_APPS

--- tech_vision/tech_vision/settings.py
+++ tech_vision/tech_vision/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuario'
+    'usuario',
+    'produtos'
 ]
 
 AUTH_USER_MODEL = 'usuario.User'
