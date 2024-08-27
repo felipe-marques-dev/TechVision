07495edfa858feb161b72d57e33a5e2084260851
Felipe <felipemarques.computacao@gmail.com>
2024-08-27 09:53:31 -0300

    Criação do models do produto

--- tech_vision/produtos/models.py
+++ tech_vision/produtos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length=40)
+    category = models.CharField(max_length=40)
+    sub_category = models.CharField(max_length=40)
+    description = models.CharField(max_length=150)
+    estoque = models.IntegerField()
+    price = models.FloatField()
+    promotion = models.BooleanField(default=False)
+    foto_1 = models.ImageField(upload_to='produtos/')
+    foto_2 = models.ImageField(upload_to='produtos/')
+    foto_3 = models.ImageField(upload_to='produtos/')
+    foto_4 = models.ImageField(upload_to='produtos/')
