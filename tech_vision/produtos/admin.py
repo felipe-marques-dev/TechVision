72ebe6e77e7b2ccec66910c77583bd79b68445d4
Felipe <felipemarques.computacao@gmail.com>
2024-08-29 08:51:30 -0300

    Endereço adicionado no painel de Admin

--- tech_vision/produtos/admin.py
+++ tech_vision/produtos/admin.py
@@ -2,10 +2,8 @@
 from django.contrib import admin
 from .models import Product
 
-
-@admin.register(Product)
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('product_id','name','category', 'sub_category','description', 'estoque', 'price', 'promotion','foto_1', 'foto_2', 'foto_3', 'foto_4')  # Campos que serão exibidos na lista de produtos
     search_fields = ('name',)  # Campos pesquisáveis no painel de admin
 
-
+admin.site.register(Product, ProdutoAdmin)
--- tech_vision/usuario/admin.py
+++ tech_vision/usuario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Adress, User
 # Register your models here.
+
+
+@admin.register(Adress)
+class AdressAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'street', 'number', 'block', 'reference', 'state', 'cep', 'city')
+    search_fields = ('user_id__email', 'street', 'city')
