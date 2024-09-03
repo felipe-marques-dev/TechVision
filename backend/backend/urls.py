797f18642484087ee5b6537658568be41373abf6
Lucas <lucasleite.miguel10@gmail.com>
2024-09-03 15:50:50 -0300

    Nova branch criada

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views import generic
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', generic.RedirectView.as_view(url='/accounts/home', permanent=False)),
     path('accounts/', include('usuario.urls')),
     path('produtos/', include('produtos.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
