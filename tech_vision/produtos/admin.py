315719a62893072feb10ee154e4e6e9c02f99ad0
Felipe <felipemarques.computacao@gmail.com>
2024-09-03 08:03:35 -0300

    feat: nome como PK para edição dos dados

--- tech_vision/produtos/admin.py
+++ tech_vision/produtos/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Product
 
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('product_id','name','category', 'sub_category','description', 'estoque', 'price', 'promotion','foto_1', 'foto_2', 'foto_3', 'foto_4')  # Campos que serão exibidos na lista de produtos
+    list_display = ('name','product_id','category', 'sub_category','description', 'estoque', 'price', 'promotion','foto_1', 'foto_2', 'foto_3', 'foto_4')  # Campos que serão exibidos na lista de produtos
     search_fields = ('name',)  # Campos pesquisáveis no painel de admin
 
 admin.site.register(Product, ProdutoAdmin)
