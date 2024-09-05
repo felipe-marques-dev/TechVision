74c612aaad8ae92bd328f7fd3453ddc62034c6c5
Felipe <felipemarques.computacao@gmail.com>
2024-09-05 14:58:53 -0300

    feat: pedidos no painel da transportadora

--- tech_vision/compra/admin.py
+++ tech_vision/compra/admin.py
@@ -7,26 +7,29 @@ class PedidoAdmin(admin.ModelAdmin):
     list_display = ('delivery_id','endereco','transportadora','user','frete')  # Campos exibidos
     search_fields = ('delivery_id','user_id')  # Campos pesquisáveis no painel de admin
 
-
+# mostra os pedidos de cada usuario
 class PedidoInline(admin.TabularInline):
     model = Pedido
     fields = ('delivery_id','status','user','endereco', 'transportadora')
     extra = 1  # Número de formulários vazios a serem exibidos
     can_delete = False  # Impede a exclusão de pedidos diretamente do painel
 
+# mostra os pedidos em cada transportadora
+class TransportadoraInline(admin.TabularInline):
+    model = Pedido
+    fields = ['frete', 'user', 'status']
+    extra = 0
+    can_delete = False
 
 @admin.register(Transportadora)
 class CarrierAdmin(admin.ModelAdmin):
     list_display = ('name', 'carrier_id', 'cnpj', 'description') # Campos exibidos
-    search_fields = ('name', 'carrier_id', 'cnpj') # Campos pesquisáveis no painel de admin
+    search_fields = ('name', 'carrier_id', 'cnpj')
+    inlines = [TransportadoraInline] # Campos pesquisáveis no painel de admin
 
 
 
 
-class PedidoInline(admin.TabularInline):
-    model = Pedido
-    extra = 1
-    
 
 
 
