bb151f1035f812ade03a22c3344ec1d18cc3a4e7
Felipe <felipemarques.computacao@gmail.com>
2024-09-12 14:53:32 -0300

    feat: API de categorias

--- backend/produtos/urls.py
+++ backend/produtos/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path, include
-from .views import ProductListCreate, ProductDetail
+from .views import ProductListCreate, ProductDetail, CategoryList
 
 urlpatterns = [
     path('itens/', ProductListCreate.as_view(), name='product-list-create'),
     path('itens/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
+    path('categories/', CategoryList.as_view(), name='categories')
 ]
--- backend/produtos/views.py
+++ backend/produtos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Produto
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer,CategorySerializer
 from rest_framework import generics, routers, serializers, viewsets
 from rest_framework import permissions, status
 from rest_framework.authentication import SessionAuthentication
@@ -21,3 +21,10 @@ class ProductListCreate(generics.ListCreateAPIView):
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Produto.objects.all()
     serializer_class = ProductSerializer
+
+class CategoryList(generics.ListCreateAPIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
+
+    queryset = Produto.objects.values('category').distinct()
+    serializer_class = CategorySerializer
