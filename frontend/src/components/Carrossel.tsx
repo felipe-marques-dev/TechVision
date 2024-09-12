aaddd5ff1704d10508c31a005678b491092d470e
Felipe <felipemarques.computacao@gmail.com>
2024-09-12 11:20:17 -0300

    feat: carrossel de produtos (100%)

---
+++ frontend/src/components/Carrossel.tsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from "react";
+import { client } from "../services/client";
+import { Link } from "react-router-dom";
+import { Carousel, CarouselItem } from "react-bootstrap";
+import { H3 } from  "../styles/Carrossel/lista";
+interface Produto{
+    name: string;
+    product_id: number;
+    price: number;
+}
+
+function Carrossel(){
+    const [produtos, setProdutos] = useState<Produto[]>([]);
+    const img = "teclado.jpg"
+
+    
+    useEffect(() => {
+        client.get('/produtos/itens')
+                .then(response =>{
+                    setProdutos(response.data)
+        })
+        .catch(error => {
+            console.log("Ops! Ocorreu um erro ao buscar os produtos.")
+        })
+    }, []);
+
+  
+    console.log(produtos)
+    return(
+      <div className="text-center">
+        <div id="texto-titulo" className="col d-flex justify-content-center fs-1 p-0 m-0" >
+            <H3 id="text-titulo">Em destaque</H3>
+          </div>
+
+
+
+                <Carousel  className="carousel carousel-dark slide">
+                    {produtos.map(produto => (
+                        <Carousel.Item key={produto.product_id}>
+                            <Link to={`/produto/${produto.product_id}`}><img src={produto.foto_1} className="w-25 p-4 m-3 mt-0" /></Link>
+                        </Carousel.Item>
+                        ))}
+                </Carousel>
+
+        </div>
+
+    );
+
+}
+
+
+export default Carrossel;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
 import React from "react"; 
-import ListaProdutos from "../../components/ListaProdutos";
+import Carrossel from "../../components/Carrossel";
 import { Navbar } from "../../components/Navbar";
 export function Home(){
     return(
         <div>
             <Navbar />
-            <ListaProdutos />
+            <Carrossel />
         </div>
         
     )
