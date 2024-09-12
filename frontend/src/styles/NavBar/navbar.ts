0458a7bab1a6f1058297677fc6a6062b23de676f
Gabriel <gabriel.dev09@gmail.com>
2024-09-12 10:33:34 -0300

    feat: Navbar (completar com dropdown e rotas)

--- frontend/src/styles/NavBar/navbar.ts
+++ frontend/src/styles/NavBar/navbar.ts
@@ -1,9 +1,22 @@
+import { useDebugValue } from "react";
 import { styled } from "styled-components";
 
 const Div = styled.a`
-    
+    background-color: black; 
     color: white; 
     font-size: 20px;
+    text-decoration: none;
+`;
+
+const Button = styled.button`
+    color: white; 
+    font-size: 20px;
+    text-decoration: none;
+`;
+
+const A = styled.a`
+    text-decoration: none;
+
 `
 
-export default Div;
+export { Div, Button, A };
