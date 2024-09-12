cb8b351a98b183fe8e813cc3cf907a0dd05b8627
Felipe <felipemarques.computacao@gmail.com>
2024-09-12 09:09:52 -0300

    feat: navbar estilizada (incompleto)

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Div from "../styles/NavBar/Navbar"
 export function Navbar(){
 
     const logo_atena = 'logo_atena.png'
@@ -6,33 +7,33 @@ export function Navbar(){
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid" id="header">
-            <a className="navbar-brand" href="index.html"><img className="logo" src={`/images/${logo_atena}`}/></a>
+            <Div className="navbar-brand" href="index.html"><img className="logo" src={`/images/${logo_atena}`}/></Div>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="index.html">Home</a>
+                <Div className="nav-link active" aria-current="page" href="index.html">Home</Div>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="conta.html" >Minha conta</a>
+                <Div className="nav-link" href="conta.html" >Minha conta</Div>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="carrinho.html" >Carrinho</a>
+                <Div className="nav-link" href="carrinho.html" >Carrinho</Div>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white; font-size: 20px;" >
+                <Div className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{}} >
                   Categorias
-                </a>
+                </Div>
                 <ul className="dropdown-menu" id="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Eletrônicos</a></li>
-                  <li><a className="dropdown-item" href="#">Planos - Tv box</a></li>
+                  <li><Div className="dropdown-item" href="#">Eletrônicos</Div></li>
+                  <li><Div className="dropdown-item" href="#">Planos - Tv box</Div></li>
                 </ul>
               </li>
             </ul>
             <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="pesquisar..." aria-label="Search">
+              <input className="form-control me-2" type="search" placeholder="pesquisar..." aria-label="Search" />
               <button className="btn" id="btn-search" type="submit">Pesquisar</button>
             </form>
           </div>
---
+++ frontend/src/styles/NavBar/navbar.ts
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+const Div = styled.a`
+    
+    color: white; 
+    font-size: 20px;
+`
+
+export default Div;
