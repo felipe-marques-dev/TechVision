464d136168e99193094604e27664263049f334d3
Felipe <felipemarques.computacao@gmail.com>
2024-09-12 09:08:19 -0300

    feat: ProdutoIndividual

--- frontend/src/Routes/routes.tsx
+++ frontend/src/Routes/routes.tsx
@@ -8,7 +8,7 @@ import { Login } from "../pages/Login/index.tsx";
 import { Cadastro } from "../pages/Cadastro/index.tsx";
 import { Home } from "../pages/Home/index.tsx";
 import { Profile } from "../pages/Profile/index.tsx";
-
+import { ProdutoIndividual } from "../components/ProdutoIndividual.tsx";
 export function AppRoutes(){
     return (
         <Router>
@@ -17,6 +17,7 @@ export function AppRoutes(){
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/produto/:id" element={<ProdutoIndividual />}/>
             </Routes>
         </Router>
     )
