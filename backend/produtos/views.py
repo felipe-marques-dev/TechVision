8adc168dfbfe7069775072f39e091b9922bfecdc
Felipe <felipemarques.computacao@gmail.com>
2024-09-12 23:01:23 -0300

    feat: verificação de permissão nas categorias

--- backend/produtos/views.py
+++ backend/produtos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Produto
 from .serializers import ProductSerializer,CategorySerializer
-from rest_framework import generics, routers, serializers, viewsets
+from rest_framework import generics
 from rest_framework import permissions, status
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.response import Response
@@ -16,23 +16,19 @@ class ProductListCreate(generics.ListCreateAPIView):
             permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-   
 
-
-# ViewSets define the view behavior.
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Produto.objects.all()
     serializer_class = ProductSerializer
 
 class CategoryList(generics.ListCreateAPIView):
-    authentication_classes = [SessionAuthentication]
-    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
-
     queryset = Produto.objects.values('category').distinct()
     serializer_class = CategorySerializer
 
-def my_view(request):
-    # Você pode acessar dados da solicitação, como:
-    user_agent = request.META.get('HTTP_USER_AGENT', 'unknown')
-    return HttpResponse(f'Your user agent is {user_agent}')
+    def check_permission(self, request):
+        if request.method == 'POST' and  permission_classes == (permissions.IsAuthenticated, permissions.IsAdminUser):
+            authentication_classes = [SessionAuthentication]
+            permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
