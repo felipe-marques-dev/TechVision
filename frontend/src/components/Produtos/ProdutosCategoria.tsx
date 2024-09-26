e6601d32449ea198703a6c1d6d65e73244244ce9
Felipe <felipemarques.computacao@gmail.com>
2024-09-26 19:32:17 -0300

    feat: produtos exibidos conforme a categoria

--- frontend/src/components/Produtos/ProdutosCategoria.tsx
+++ frontend/src/components/Produtos/ProdutosCategoria.tsx
@@ -1,25 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Produto } from "../../types/Produto";
 import { AxiosResponse } from "axios";
 import { client } from "../../services/client";
 import { useParams } from "react-router-dom";
+import ProdutoCard from "./ProdutoCard";
 
 
 export default function ProdutosCategoria(){
-    const [produtos, setProdutos] = useState<Produto | null>(null)
+    const [produto, setProdutos] = useState<Produto[]>([]);
     const { name } = useParams<{name: string}>()
 
-    const loadProdutos = async () => {
-        try {
-            const response: AxiosResponse<Produto> = await client.get(`categories-filter/${name}`);
-            setProdutos(response.data)
-        }catch(error){
-            return console.log("ERRO")
-        }
-    }
+    useEffect(() => {
+        const loadProdutos = async () => {
+            try {
+                const response: AxiosResponse<Produto[]> = await client.get(`produtos/categories-filter/${name}`);
+                console.log(response.data);
+                setProdutos(response.data.products);
+                console.log(produto);
+            } catch (error) {
+                console.log("ERRO", error);
+            }
+        };
 
-    loadProdutos
-    return(
-        <h1>{name}</h1>
-    )
+        loadProdutos(); // Chama a função loadProdutos ao montar o componente ou quando "name" mudar
+    }, [name]); 
+    
+    return (
+        <div className="container my-4">
+            <h2 className="text-center mb-4">Produtos</h2>
+            <div className="row g-4"> {/* g-4 para espaçamento consistente */}
+                {produto.map(produtos => (
+                    <div className="col-6 col-md-4 col-lg-3" key={produtos.url_name}> {/* Colunas responsivas */}
+                        <ProdutoCard 
+                            url_name={produtos.url_name} 
+                            price={produtos.price} 
+                            foto_1={produtos.foto_1} 
+                            description={produtos.description} 
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+    
 }
