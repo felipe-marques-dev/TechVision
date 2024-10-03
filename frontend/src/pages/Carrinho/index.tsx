cdfdf0d71ababfda10ef2d8529e182ef2c6b5c22
Gabriel <gabriel.dev09@gmail.com>
2024-10-03 11:19:39 -0300

    Fix:Carrinho (incompleto)

--- frontend/src/pages/Carrinho/index.tsx
+++ frontend/src/pages/Carrinho/index.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { Nav_bar } from "../../components/NavBar/Navbar";
 import { client } from "../../services/client";
 import { useNavigate } from "react-router-dom";
-import '../../styles/Carrinho/carrinho.css'
-import { H3 } from  "../../styles/Carrossel/lista";
+import '../../styles/Carrinho/carrinho.css';
+import { H3 } from "../../styles/Carrossel/lista";
 import Calculo from "./Calculo";
 
 interface Produto {
@@ -19,6 +19,7 @@ export function Carrinho() {
   const navigate = useNavigate();
   const [currentUser, setCurrentUser] = useState<boolean>(false);
   const [produtos, setProdutos] = useState<Produto[]>([]);
+  const [emailUser, setEmail] = useState<string>('');
 
   // Verificar sessão do usuário
   useEffect(() => {
@@ -26,17 +27,7 @@ export function Carrinho() {
     client.get("/accounts/usuario")
       .then(response => {
         setCurrentUser(true);
-        console.log(response.data.user.email)
-        // Carregar produtos quando o usuário estiver autenticado
-        client.post('/accounts/cart/', {
-          'email': response.data.user.email
-        })
-          .then(response => {
-            setProdutos(response.data);
-          })
-          .catch(error => {
-            console.log("Erro ao buscar produtos", error);
-          });
+        setEmail(response.data.user.email);
       })
       .catch(error => {
         setCurrentUser(false);
@@ -44,60 +35,72 @@ export function Carrinho() {
       });
   }, [navigate]);
 
+  
+  useEffect(() => {
+    if (emailUser) {
+      client.post('/accounts/cart/', { email: emailUser })
+        .then(response => {
+          setProdutos(response.data.itens); 
+          console.log(response.data.itens); 
+        })
+        .catch(error => {
+          console.log("Erro ao buscar produtos", error);
+        });
+    }
+  }, [emailUser]);
+
+    console.log(produtos.length)
   return (
     <div>
       <Nav_bar />
       {currentUser && (
-    
+        <div className="container-fluid">
+          <div className="col d-flex justify-content-center fs-1 ">
+            <H3 id="text-titulo" style={{ padding: 0 }}>Carrinho</H3>
+          </div>
 
-          <div className="container-fluid">
-             <div className="col d-flex justify-content-center fs-1 " >
-                        <H3 id="text-titulo" style={{padding:0}}>Carrinho</H3>
-                </div>
+          <div className="row">
+            <div className="col-8">
+              {produtos.length  >= 0 ? (
+                produtos.map(produto => (
+                  <div className="cart-item row align-items-center" key={produto.product_id}>
+                    <div className="col-md-3">
+                      <img src={produto.foto_1} alt={produto.name} />
+                    </div>
+                    <div className="col">
+                      <p className="nome-carrinho">{produto.name}</p>
+                      <p>{produto.description}</p>
+                    </div>
 
-            <div className="row">
-              <div className="col-8">
-                {produtos.length > 0 ? (
-                  produtos.map(produto => (
-                    <div className="cart-item row align-items-center" key={produto.product_id}>
-                      <div className="col-md-3">
-                        <img src={produto.foto_1} alt={produto.name} />
-                      </div>
-                      <div className="col">
-                        <p className="nome-carrinho">{produto.name}</p>
-                        <p>{produto.description}</p>
-                      </div>
-                     
-                      <div className="col text-center">
-                        <div className="row" style={{width:200}}>
+                    <div className="col text-center">
+                      <div className="row" style={{ width: 200 }}>
                         <p>R$ {produto.price}</p>
                         <button className="btn btn-danger btn-sm">Remover</button>
                       </div>
                     </div>
-                    </div>
-                  ))
-                ) : (
-                  <p>Não há produtos no carrinho.</p>
-                )}
-              </div>
+                  </div>
+                ))
+              ) : (
+                <p>Não há produtos no carrinho.</p>
+              )}
+            </div>
 
-              <div className="col d-flex" style={{padding:80}}>
-                <div className="summary">
-                  <h5>RESUMO</h5>
-                  <p><strong>Valor dos Produtos:</strong> R$ 4.899,80</p>
-                  <p><strong>Frete:</strong> R$ 0,00</p>
-                  <h5>ENTREGA</h5>
-                  <Calculo />
+            <div className="col d-flex" style={{ padding: 80 }}>
+              <div className="summary">
+                <h5>RESUMO</h5>
+                <p><strong>Valor dos Produtos:</strong> R$ 4.899,80</p>
+                <p><strong>Frete:</strong> R$ 0,00</p>
+                <h5>ENTREGA</h5>
+                <Calculo />
 
-                  <a href="pagamento.html">
-                    <button className="botao">Ir para o pagamento</button>
-                  </a>
-                  <button className="botao-carrinho">Continuar comprando</button>
-                </div>
+                <a href="pagamento.html">
+                  <button className="botao">Ir para o pagamento</button>
+                </a>
+                <button className="botao-carrinho">Continuar comprando</button>
               </div>
             </div>
           </div>
-
+        </div>
       )}
     </div>
   );
